07248f592812fc0493006ef906c0a4c4dbacf36e
Gary Biggs <Gary_Biggs@ultimatesoftware.com>
2020-01-05 19:22:41 -0800

    - Updated python_cloud_builder/Dockerfile to update pip - Updated cloudbuild.yaml to remove pylint - Updated tests/test_my_module.py to create test that will fail

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -25,32 +25,35 @@ from src.my_module import *
 
 class TestIsNumeric(object):
     def test_int(self):
-        assert is_numeric(1)==True
+        assert is_numeric(1) is True
 
     def test_float(self):
-        assert is_numeric(3.14)==True
+        assert is_numeric(3.14) is True
 
     def test_string(self):
-        assert is_numeric("abba")==False
+        assert is_numeric("abba") is False
 
     def test_bool(self):
-        assert is_numeric(True)==False
+        assert is_numeric(True) is False
 
 
 class TestAdd(object):
     """This class bundles the tests for the add function in my_module."""
 
+    # def test_add_basic(self):
+    #     assert add(1, 2) == 3
+
     def test_add_basic(self):
-        assert add(1, 2)==3
+        assert add(1, 2) == 10
 
     def test_add_zero(self):
-        assert add(0, 3)==3
+        assert add(0, 3) == 3
 
     def test_add_negative(self):
-        assert add(1, -2)==-1
+        assert add(1, -2) == -1
 
     def test_float(self):
-        assert add(1.1, 3.3)==4.4
+        assert add(1.1, 3.3) == 4.4
 
     def test_string(self):
         with pytest.raises(TypeError):
@@ -69,13 +72,13 @@ class TestSquare(object):
     """This class bundles the tests for the square function in my_module."""
 
     def test_square_positive(self):
-        assert square(2)==4
+        assert square(2) == 4
 
     def test_square_zero(self):
-        assert square(0)==0
+        assert square(0) == 0
 
     def test_square_negative(self):
-        assert square(-1)==1
+        assert square(-1) == 1
 
     def test_square_string(self):
         with pytest.raises(TypeError):
@@ -94,7 +97,7 @@ class TestLogTransform(object):
 
         log_transform() can be called without const, defaulting the const to 1. This test verifies default operation.
         """
-        assert log_transform(10)==pytest.approx(2.397, 0.001)
+        assert log_transform(10) == pytest.approx(2.397, 0.001)
 
     def test_log_transform_10_const(self):
         """Test with const argument supplied.
@@ -102,7 +105,7 @@ class TestLogTransform(object):
         log_transform() takes a positional argument 'const' that defaults to 1 and can be overridden by the user. This test
         makes sure const is implemented correctly.
         """
-        assert log_transform(10, const=.001)==pytest.approx(2.303, 0.001)
+        assert log_transform(10, const=.001) == pytest.approx(2.303, 0.001)
 
     def test_log_transform_negative_const(self):
         """Test without const argument supplied.
